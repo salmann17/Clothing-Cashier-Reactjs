99eb3bad4d797974bac54f7166962c4a52b84418
Salman Alfarizi <salmanlfaa11@gmail.com>
2024-10-18 22:31:31 +0700

    add axios.delete in success.jsx

--- src/pages/success.jsx
+++ src/pages/success.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
 import { Button, Image } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
+import { API_URL } from '../utils/constant';
 
 export default class success extends Component {
+    componentDidMount(){
+        axios.get(API_URL + "keranjangs")
+            .then(res => {
+                const keranjangs = res.data;
+                keranjangs.map(function(item){
+                    return axios.delete(API_URL + "keranjangs/" + item.id)
+                    .then((res) => console.log(res))
+                    .catch((error) => console.log(error))
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
     render() {
         return (
             <div className='mt-4 text-center'>
